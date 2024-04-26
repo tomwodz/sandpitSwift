00e6992c9eee1fa2e4d8e955bc81245b78b17fc2
tomwodz <twodzinski@gmail.com>
2024-04-26 22:42:55 +0200

    Initial Commit

---
+++ sandpitSwift/sandpitSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandpitSwiftApp.swift
+//  sandpitSwift
+//
+//  Created by Mini-M1 on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct sandpitSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
